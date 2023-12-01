fbad3e02c347cd8b3e7e302ef83ce6a5b0252e99
betech-tynt <149549668+betech-tynt@users.noreply.github.com>
2023-12-01 15:02:24 +0700

    [Training_yii] Create migrate DB

---
+++ console/migrations/m231201_072414_product.php
@@ -0,0 +1,55 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m231201_072414_product
+ */
+class m231201_072414_product extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // https://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%product}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'description' => $this->text(),
+            'price' => $this->decimal(10, 2)->notNull(),
+            'created_at' => $this->dateTime(),
+            'updated_at' => $this->dateTime(),
+        ], $tableOptions);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m231201_072414_product cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m231201_072414_product cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
